669cc0d6c3c23f8f01b0d089f72139e46ff6f04a
Bisrat-Aregawi <bisrat_aregawi1914@yahoo.com>
2022-04-19 13:07:19 +0300

    Make an annotated function that returns concatenate of two strings
    For task 1 of Solo-Project: - `0x00-python_variable_annotations`

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Module defines type-annotated `concat` function"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """Return concatenate of `str1` and `str2`"""
+    return str1 + str2
