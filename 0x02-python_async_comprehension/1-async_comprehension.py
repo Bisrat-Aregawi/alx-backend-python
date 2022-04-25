ccc0c06ee5d7006263517dc542e58f5486f847f7
Bisrat-Aregawi <bisrat_aregawi1914@yahoo.com>
2022-04-25 19:16:42 +0300

    Return list of randoms using list comprehension
    Asynchronously create a list with list comprehension
    
    For task 1 of Solo-Project: - `0x02-python_async_comprehension`

---
+++ 0x02-python_async_comprehension/1-async_comprehension.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""Module defines `async_comprehension` coroutine"""
+from typing import List
+async_generator = __import__('0-async_generator').async_generator
+
+
+async def async_comprehension() -> List[float]:
+    """Return list of 10 random numbers"""
+    return [i async for i in async_generator()]
